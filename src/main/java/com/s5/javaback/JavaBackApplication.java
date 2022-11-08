ddee18569c71ca6c01f6c492d413cac1689b57b8
Abel3581 <marzoa3581@gmail.com>
2022-11-08 11:46:52 -0300

    first commit

---
+++ src/main/java/com/s5/javaback/JavaBackApplication.java
@@ -0,0 +1,13 @@
+package com.s5.javaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s5/javaback/JavaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s5.javaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
