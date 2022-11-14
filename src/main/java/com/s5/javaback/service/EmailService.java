97cfe5f540e63cc2b298b636a5f2db359962239b
Abel3581 <marzoa3581@gmail.com>
2022-11-14 16:59:39 -0300

    agregando servicio y dependencias

---
+++ src/main/java/com/s5/javaback/service/EmailService.java
@@ -0,0 +1,25 @@
+package com.s5.javaback.service;
+
+
+import com.s5.javaback.model.request.EmailRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.internet.MimeMessage;
+
+@RequiredArgsConstructor
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    private final SpringTemplateEngine templateEngine;
+
+   public void sendHtmlMessage(EmailRequest email){
+       MimeMessage message = javaMailSender.createMimeMessage();
+   }
+
+
+}
