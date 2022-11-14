4cf9b1b1a0b49c6fe4db248bc211c982c8f7b8b6
Alben <albenbustamante@gmail.com>
2022-11-14 11:56:12 -0500

    configurados los cors

--- src/main/java/com/s5/javaback/security/SecurityConfig.java
+++ src/main/java/com/s5/javaback/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -46,6 +48,18 @@ public class SecurityConfig {
                 .anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+            }
+        };
     }
 
 }
