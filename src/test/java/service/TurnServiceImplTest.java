406e685ec95e405a87a93c51656aff78ec1c3494
Abel3581 <marzoa3581@gmail.com>
2022-11-28 23:39:00 -0300

    act

---
+++ src/test/java/service/TurnServiceImplTest.java
@@ -0,0 +1,23 @@
+package service;
+
+import com.s5.javaback.model.entity.Turn;
+import org.junit.jupiter.api.Test;
+
+
+
+class TurnServiceImplTest {
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void deleted() {
+    }
+
+    @Test
+    void save() {
+        Turn turn = new Turn();
+    }
+}
