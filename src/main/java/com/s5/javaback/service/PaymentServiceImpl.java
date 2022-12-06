c7132bec61fbbdd1e6790afb6222d78524fe09ea
Abel3581 <marzoa3581@gmail.com>
2022-12-05 22:53:32 -0300

    agregado endpoint pagar con  mercado pago

--- src/main/java/com/s5/javaback/service/PaymentServiceImpl.java
+++ src/main/java/com/s5/javaback/service/PaymentServiceImpl.java
@@ -1,8 +1,12 @@
 package com.s5.javaback.service;
 
 import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.preference.PreferenceBackUrlsRequest;
 import com.mercadopago.client.preference.PreferenceClient;
 import com.mercadopago.client.preference.PreferenceItemRequest;
+import com.mercadopago.client.preference.PreferenceRequest;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.preference.Preference;
 import com.s5.javaback.model.entity.Turn;
 import com.s5.javaback.service.abstraction.PaymentService;
@@ -25,7 +29,7 @@ public class PaymentServiceImpl implements PaymentService {
     private TurnService turnService;
 
     @Override
-    public Preference createPreference(Long turnId) {
+    public Preference createPreference(Long turnId) throws MPException, MPApiException {
         Turn turn = turnService.findById(turnId);
         //Le paso el token de la aplicacion
         MercadoPagoConfig.setAccessToken(accessToken);
@@ -37,11 +41,24 @@ public class PaymentServiceImpl implements PaymentService {
                 .title(turn.getHomeEvent().getName())
                 .quantity(1)
                 .unitPrice(BigDecimal.valueOf(turn.getPrice()))
-                .description("Turno para la cas" + turn.getHomeEvent().getName())
+                .description("Turno para la casa " + turn.getHomeEvent().getName())
                 .currencyId("ARS")
                 .build();
         items.add(item);
+        //Url de respuestas
+        PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
+                //Url de respuesta cuando se apreta el boton volver al sitio
+                .success("http://localhost:8080/payment/success")
+                .failure("http://localhost:8080/payment/failure")
+                .pending("http://localhost:8080/payment/pending")
+                .build();
 
-        return null;
+        PreferenceRequest request = PreferenceRequest.builder()
+                .items(items)
+                .backUrls(backUrls)
+                .build();
+        Preference response = preferenceClient.create(request);
+
+        return response;
     }
 }
