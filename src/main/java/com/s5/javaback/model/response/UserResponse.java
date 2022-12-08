6de528a5bd681c766aba3d7c2b8205c7656de47e
Alben <albenbustamante@gmail.com>
2022-12-08 18:37:56 -0500

    Agregado estado de usuario a la respuesta

--- src/main/java/com/s5/javaback/model/response/UserResponse.java
+++ src/main/java/com/s5/javaback/model/response/UserResponse.java
@@ -1,5 +1,6 @@
 package com.s5.javaback.model.response;
 
+import com.s5.javaback.util.enums.UserStatus;
 import lombok.Data;
 
 @Data
@@ -7,4 +8,5 @@ public class UserResponse {
     private final long id;
     private final String name, email, createdAt;
     private final ImageResponse image;
+    private final UserStatus status;
 }
